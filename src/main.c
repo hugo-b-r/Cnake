7f3d2fa7c73603a2559010240f56fd026ae99eda
LeMoustachu <hugoberthetrambaud@outlook.com>
2022-02-14 17:10:10 +0100

    working

--- src/main.c
+++ src/main.c
@@ -9,136 +9,108 @@ main file for game
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <conio.h>
+
+#define PLAYGROUND_X 50
+#define PLAYGROUND_Y 30
+
+void newPos(int *x, int *y, int orientation)
+{
+    switch (orientation) {
+    case 0:
+        if (*y != PLAYGROUND_Y - 1) {
+            *y += 1;
+        }
+        break;
+    case 90:
+        if (*x != PLAYGROUND_X - 1) {
+            *x += 2;
+        }
+        break;
+    case 180:
+        if (*y != 0) {
+            *y -= 1;
+        }
+        break;
+    case 270:
+        if (*x != 0) {
+            *x -= 2;
+        }
+    }
+}
 
 
-char getCommand();
+void clearBuffer(char (*buffer)[PLAYGROUND_X][PLAYGROUND_Y])
+{
+    for (int i = PLAYGROUND_Y - 1; i > 0; i--) {
 
-int newX (int orientation, int oldX);
+        for (int j = 0; j < PLAYGROUND_X; j++) {
+            (*buffer)[j][i] = '_';
+        }
+    }
+}
 
-int newY (int orientation, int oldY);
 
-void clearBuffer(char (*buffer)[10][10]);
 
-void printBuffer(char buffer[10][10]);
+void printBuffer(char (*buffer)[PLAYGROUND_X][PLAYGROUND_Y])
+{
+    for (int i = PLAYGROUND_Y - 1; i > 0; i--) {
+
+        for (int j = 0; j < PLAYGROUND_X; j++) {
+            printf("%c", (*buffer)[j][i]);
+        }
+
+        printf("\n");
+    }
+}
+
 
 
 int main(int argc, char* argv[])
 {
     char command = 'a';
 
-    char buffer[10][10];
+    char buffer[PLAYGROUND_X][PLAYGROUND_Y];
     clearBuffer(&buffer);
 
-    int positions[2][10];
+    int x = 0;
+    int y = 0;
 
-    int tail_position = 0;
-    int head_position = 0;
-    int length = 1;
+    int orientation = 0;
 
     while (command != 'x') {
 
-        command = getCommand();
+        if (kbhit()) {
+            switch (getch()) {
+            case 'z':
+                orientation = 0;
+                break;
+
+            case 'q':
+                orientation = 270;
+                break;
+
+            case 's':
+                orientation = 180;
+                break;
+
+            case 'd':
+                orientation = 90;
+                break;
+
+            case 'x':
+                return EXIT_SUCCESS;
+            }
+        }
+
+        newPos( &x, &y, orientation );
+
+        clearBuffer(&buffer);
+
+        buffer[x][y] = 'o';
+
         system("cls");
 
-        switch (command) {
-        case 'z':
-            orientation = 0;
-            break;
-
-        case 'q':
-            orientation = 270;
-            break;
-
-        case 's':
-            orientation = 180;
-            break;
-
-        case 'd':
-            orientation = 90;
-            break;
-        }
-
-        newPos( &(positions[0][tail_position]), &(positions[1][tail_position]), orientation  );
-
-        tail_position += 1;
-        head_position += 1;
-
-        clearBuffer(&(buffer));
-
-
-        for(int i = 0; i < length; i++){
-
-            buffer[positions[0][i]][positions[1][i]] = 'o';
-
-        }
-
-        printBuffer(buffer);
-
-    }
-
-    return EXIT_SUCCESS;
-
-}
-
-
-
-
-char getCommand()
-{
-    #if (defined(_WIN32) || defined(WIN32))
-
-        #include <conio.h>
-
-        if (kbhit()) {
-            return geth();
-        }
-
-    #elif defined(linux)
-
-            return system("read -n1");
-
-    #endif
-}
-
-
-void newPos(int *x, int *y, int orientation)
-{
-    switch (orientation) {
-    case 0:
-        *x += 1;
-        break;
-    case 90:
-        *y += 1;
-        break;
-    case 180:
-        *x -= 1;
-        break;
-    case 270:
-        *y -= 1;
-    }
-}
-
-
-void clearBuffer(char (*buffer)[10][10])
-{
-    for(int i = 0; i < 10; i++){
-
-        for(int j = 0; j < 10; j++){
-            *buffer[i][j] = ' ';
-        }
-    }
-}
-
-
-
-void printBuffer(char buffer[10][10])
-{
-    for(int i = 0; i < 10; i++){
-
-        for(int j = 0; j < 10; j++){
-            printf("%c", buffer[i][j]);
-        }
-
-        printf("\n");
+        printBuffer(&buffer);
     }
 }
