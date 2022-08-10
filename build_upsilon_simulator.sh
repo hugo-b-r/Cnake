e52494317750b4a105968299e88a919dd279fc80
Hugo Berthet-Rambaud <hugoberthetrambaud@outlook.com>
2022-08-10 13:54:52 +0200

    update build script

--- build_upsilon_simulator.sh
+++ build_upsilon_simulator.sh
@@ -6,3 +6,10 @@ git clone --recursive https://github.com/UpsilonNumworks/Upsilon.git
 cp -r Cnake/inc Upsilon/apps/external/app
 cp Cnake/*.c Upsilon/apps/external/app
 cp Cnake/sources.mak Upsilon/apps/external/app
+
+wget https://raw.githubusercontent.com/lemoustachu/Upsilon-External/Cnake/apps/Cnake/Makefile -O Upsilon/apps/external/app/Makefile
+wget https://raw.githubusercontent.com/lemoustachu/Upsilon-External/Cnake/apps/Cnake/inc/init.h -O Upsilon/apps/external/app/inc/init.h
+
+cd Upsilon
+make cleanall
+make PLATFORM=simulator -j8 #you may want to configure building processes here.
