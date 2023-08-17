14ee2099a1a8015f5a4802bbe7fd8f67f67a6016
Hugo Berthet-Rambaud <hugoberthetrambaud@outlook.com>
2023-08-17 10:04:21 +0200

    add some new default files for platform specific functions

---
+++ linux/console.c
@@ -0,0 +1,28 @@
+/*
+ *
+ * console.c
+ *
+ * */
+
+void print_at(int x, int y, char * text) {
+
+}
+
+// to get a character but non blocking
+char get_character() {
+	
+}
+
+void clear_screen() {
+
+}
+
+int screen_x() {
+
+}
+
+int screen_y() {
+
+}
+
+
