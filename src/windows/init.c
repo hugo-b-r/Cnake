1c0975a50bff0877c839556450fcf0fb43d01895
Hugo Berthet-Rambaud <hugoberthetrambaud@outlook.com>
2023-08-18 10:52:34 +0200

    some windows functions

--- src/windows/init.c
+++ src/windows/init.c
@@ -10,24 +10,9 @@ function for init and deinit
 #include <stdlib.h>
 #include <time.h>
 
+void init_platform() { ; }
 
-
-
-void quit(int type)
-{
-	#if defined(__linux__)
-		#include <ncurses.h>
-		endwin();
-	#endif
-    switch (type) {
-        case 1:
-            printf("\nOuch!\n");
-            break; 
-        case 2: 
-            printf("\nGood Job!\n");
-            break;
-        case 3:
-            printf("\nBye bye !\n\n");
-    }
-    exit(0);
+void gracefully_shutdown(char *message) {
+  printf("%s", message);
+  exit(0); 
 }
--- src/windows/platform.c
+++ src/windows/platform.c
@@ -4,30 +4,58 @@
  *
  * */
 #include "main.h"
+#include <conio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <stdio.h>
 
-void print_at(int x, int y, char * text) {
-
+void print_at(int x, int y, char *text) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+    printf("%s", text);
 }
 
 // to get a character but non blocking
 Control get_control() {
-	
+    if (kbhit()) {
+        char event = getch();
+        switch (event) {
+        case 'z':
+            return up;
+        case 'q':
+            return left;
+        case 's':
+            return down;
+        case 'd':
+            return right;
+        case 'p':
+            return pause;
+        case 'x':
+            return end_game;
+        case 'k':
+            return quit;
+        }
+    }
 }
 
-void clear_screen() {
-
-}
+void clear_screen() { system("cls"); }
 
 int screen_x() {
-
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int columns;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+    return columns;
 }
 
 int screen_y() {
-
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int rows;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    columns = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+    return rows;
 }
 
-void uni_sleep() {
- 
-}
-
-
+void uni_sleep(int time_ms) { sleep(time_ms); }
