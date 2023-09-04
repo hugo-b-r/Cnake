67b78ab52bfc8b5cf5092b303e44b5e047bee856
Hugo Berthet-Rambaud <hugoberthetrambaud@outlook.com>
2023-09-04 08:32:02 +0200

    Remove unuseful function

--- src/game.c
+++ src/game.c
@@ -125,9 +125,48 @@ void game(int *level, int playground_width, int playground_height, int *game_con
         } else {
             draw_sth(positions[0][tailPosition(head_position, current_length)], positions[1][tailPosition(head_position, current_length)], nothing_dr);
         }
+        // Compute the new position of the head
+        int x = positions[0][head_position];
+        int y = positions[1][head_position];
+        // Convert orientation to coordinates change
+        switch (orientation) {
+        case north:
+            y -= 1; // Remember coordinates are "reversed"
+            break;
+        case west:
+            x -= 1; // i.e.
+            break;
+        case south:
+            y += 1; // i.e.
+            break;
+        case east:
+            x += 1; // i.e.
+            break;
+        }
 
-        newPos(&head_position, &positions, orientation, current_length, &control, playground_width, playground_height);
-        if (control == end_game) return; // to immediately engame if new pos not good
+        //if on itself
+        for (int i = 0; i < current_length; i++) {
+            if ( (x == positions[0][i]) && (y == positions[1][i]) ) {
+                newPosStorage(&head_position, current_length, &positions, x, y);
+                clear_screen();
+                print_at(0, 0, "Ouch. You died eating yourself.");
+                control = end_game;
+                return; // Engame if new pos not good
+            }
+        }
+
+        // if border
+        if ( (x > (playground_width-1)) || (y > (playground_height-1)) || (x < 0) || (y < 0) ) {
+            newPosStorage(&head_position, current_length, &positions, x, y);
+            clear_screen();
+            print_at(0, 0, "You died crushed against the wall.");
+            control = end_game;
+            return; // Engame if new pos not good
+        } else {
+
+            newPosStorage(&head_position, current_length, &positions, x, y);
+            incrPos(&head_position, current_length);
+        }
         draw_sth(positions[0][head_position], positions[1][head_position], snake_body);
 
         //if on fruit
